7349d55be5512121c82faae7633dc08e01ec1f95
hyeonmin11 <99racer@naver.com>
2023-03-09 19:14:11 +0900

    final version

--- Desktop/workspace/oss/main.py
+++ Desktop/workspace/oss/main.py
@@ -2,9 +2,11 @@
 def twoSum(lst, target):
     storei=0
     storej=0
-
+    
     for i in range(len(lst)):
+        
         for j in range(i+1, len(lst)):
+         
             if lst[i] + lst[j] == target:
                 storei = i
                 storej = j
@@ -13,9 +15,9 @@ def twoSum(lst, target):
     
     return 
 ll = []
-templl = list(input('input list value').split())
-ll.append(templl)
-tg = list(input('input target'))
+ll = list(map(int, input('input list value ').split()))
+tg = int(input('input target '))
+
 twoSum(ll, tg)
 
     
